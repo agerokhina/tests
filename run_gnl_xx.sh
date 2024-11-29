4393cdae1a8f6da934e5818200c5f08eb61b9cb0
Alina Erokhina <aerokhin@f0r1s1.codam.nl>
2024-11-29 18:20:30 +0100

    test v.1.1

--- run_gnl_xx.sh
+++ run_gnl_xx.sh
@@ -1,3 +1,3 @@
 PATHH=$(awk -v pwd="$PWD" 'BEGIN {printf("%s/get_next_line.h", pwd)}')
-cc  -Wall -Wextra -Werror -fsanitize=address -g ../tests_mains/main_gnl.c get_next_line.c get_next_line_utils.c -D BUFFER_SIZE=$2 -D PATHH=\"$PATHH\"  -D VAL=$1 -o ../tests_mains/testgnl.out
-../tests_mains/testgnl.out
+cc  -Wall -Wextra -Werror -fsanitize=address -g ../tests/main_gnl.c get_next_line.c get_next_line_utils.c -D BUFFER_SIZE=$2 -D PATHH=\"$PATHH\"  -D VAL=$1 -o ../tests/testgnl.out
+../tests/testgnl.out
--- run_printf.sh
+++ run_printf.sh
@@ -1,3 +1,3 @@
 PATHH=$(awk -v pwd="$PWD" 'BEGIN {printf("%s/ft_printf.h", pwd)}')
-cc -Wall -Wextra -fsanitize=address -g ../tests_mains/main_printf.c *.c -L./libft -lft -D PATHH=\"$PATHH\" -o ../tests_mains/testpf.out
-../tests_mains/testpf.out
+cc -Wall -Wextra -fsanitize=address -g ../tests/main_printf.c *.c -L./libft -lft -D PATHH=\"$PATHH\" -o ../tests/testpf.out
+../tests/testpf.out
