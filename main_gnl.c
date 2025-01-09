10c032dd06f060fa084170742e645236ac44c465
agerokhina <a.g.erohina@gmail.com>
2025-01-09 18:32:33 +0100

    minor fixes

--- main_gnl.c
+++ main_gnl.c
@@ -6,13 +6,34 @@
 /*   By: aerokhin <aerokhin@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 13:31:18 by aerokhin          #+#    #+#             */
-/*   Updated: 2024/12/05 13:28:17 by aerokhin         ###   ########.fr       */
+/*   Updated: 2025/01/09 18:31:55 by aerokhin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include PATHH
+#include "get_next_line.h"
 #include <fcntl.h>
 #include <unistd.h>
+
+# ifndef VAL
+#  define VAL 1
+# endif
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+
+size_t	ft_strlen_test(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
 
 static char	*ft_strjoin_test(char const *s1, char const *s2)
 {
@@ -23,8 +44,8 @@ static char	*ft_strjoin_test(char const *s1, char const *s2)
 
 	if ((!s1 || !s2))
 		return (NULL);
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
+	len1 = ft_strlen_test(s1);
+	len2 = ft_strlen_test(s2);
 	dest = malloc((len1 + len2 + 1) * sizeof(char));
 	if (!dest)
 		return (NULL);
@@ -55,24 +76,40 @@ static char	*ft_free_test(char **ptr)
 
 int main(void)
 {
-	int fd;
-	char *line;
-	int i;
-	char *file;
+	int		fd;
+	char	*line;
+	int		i;
+	char	*file;
+	int		i_max;
+
 # if VAL == 1
-	char files[][50] = {"41_with_nl" , "41_no_nl" , "42_no_nl" , "42_with_nl" , "43_no_nl" , "43_with_nl" , "alternate_line_nl_no_nl" , "alternate_line_nl_with_nl" , "big_line_no_nl" , "big_line_with_nl" , "empty" , "multiple_line_no_nl" , "multiple_line_with_nl" , "multiple_nlx5" , "nl", "giant_line_nl.txt", "giant_line.txt"};
-	int	i_max = 17;
+
+	char files[][50] = {"41_with_nl", "41_no_nl", "42_no_nl", "42_with_nl", \
+						"43_no_nl", "43_with_nl", "alternate_line_nl_no_nl", \
+						"alternate_line_nl_with_nl", "big_line_no_nl", 
+						"big_line_with_nl", "empty", "multiple_line_no_nl", \
+						"multiple_line_with_nl", "multiple_nlx5", "nl", \
+						"giant_line_nl.txt", "giant_line.txt"};
+	i_max = 17;
 # elif VAL == 2
-	char files[][50] = {"limit.txt:", "limit.txt;", "limit.txt<", "limit.txt=", "limit.txt>", "limit.txt?", "limit.txt0", "limit.txt1", "limit.txt2", "limit.txt3", "limit.txt4", "limit.txt5", "limit.txt6", "limit.txt7", "limit.txt8", "limit.txt9"};
-	int	i_max = 16;
+
+	char files[][50] = {"limit.txt:", "limit.txt;", \
+						"limit.txt<", "limit.txt=", "limit.txt>", "limit.txt?", \
+						"limit.txt0", "limit.txt1", "limit.txt2", "limit.txt3", \
+						"limit.txt4", "limit.txt5", "limit.txt6", "limit.txt7", \
+						"limit.txt8", "limit.txt9"};
+	i_max = 15;
 # else
+
 	char files[1][1];
-	int i_max;
+	i_max = 0;
+	
 # endif
 
 	if (VAL == 1 || VAL == 2)
 	{
-		printf("\nGet Next Line test:\n   Buffer size: %i\n   List #%i\n--------------------------", BUFFER_SIZE, VAL);
+		printf("\nGet Next Line test:\n  Buffer size: %i\n  List #%i\n----", \
+										BUFFER_SIZE, VAL);
 		i = 0;
 		while (i < i_max)
 		{
@@ -80,7 +117,6 @@ int main(void)
 			fd = open(file, O_RDONLY);
 			printf("\n\nFile(%i): '%s'\n", i, files[i]);
 			line = get_next_line(fd);
-				// printf("%p", line);
 			while (line != NULL)
 			{
 				printf("%s", line);
